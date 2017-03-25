29432ae3ab0b151119e87d4a28c67f11f61aca41
Rajesh <rajkatie@gmail.com>
2017-03-25 17:47:35 +0530

    solution for LSP

---
+++ LSP/6 after/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LSP._6_after
+{
+   public class Square: Shape
+    {
+        public int Sides;
+        public int Area()
+        {
+            return Sides * Sides;
+        }
+    }
+}
